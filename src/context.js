5bb32183a4d2f701782b93509da5f95dd48804ee
Erik <000erik001@gmail.com>
2021-12-21 17:23:03 +0400

    first-commit

---
+++ src/context.js
@@ -0,0 +1,25 @@
+import React, { useState, useContext } from "react";
+
+import useFetch from "./useFetch";
+// make sure to use https
+export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`;
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+ 
+  const [query, setQuery] = useState("spider");
+  const {isLoading,error,data:movies} = useFetch(`&s=${query}`);
+  
+  return (
+    <AppContext.Provider value={{ isLoading, error, movies, query, setQuery }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+// make sure use
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
